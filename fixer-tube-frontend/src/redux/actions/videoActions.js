0306cd40de19fe7cc1ab9c75741b25066d1868c3
Nick <github email address>
2020-11-28 22:09:43 -0500

    added addVideo action

--- fixer-tube-frontend/src/redux/actions/videoActions.js
+++ fixer-tube-frontend/src/redux/actions/videoActions.js
@@ -1,4 +1,5 @@
 const LOAD_DATA = 'LOAD_DATA';
+const ADD_VIDEO = 'ADD_VIDEO';
 
 export const loadData = () => {
     return (dispatch) => {
@@ -6,4 +7,22 @@ export const loadData = () => {
         .then(resp => resp.json())
         .then(data => dispatch({type: LOAD_DATA, payload: data}))
     }
-}
+};
+
+export const addVideo = (newVideo,history) => {
+    return (dispatch) => {
+        fetch('http://localhost:3001/videos',{
+            method: 'post',
+            headers: {
+                'Content-Type':'application/json',
+                'Accepts':'application/json'
+            },
+            body: JSON.stringify(newVideo)
+        })
+        .then(resp => resp.json())
+        .then(newVideoObj => {
+            dispatch({type: ADD_VIDEO, payload: newVideoObj})
+            history.push(`videos/${newVideoObj.slug}`)
+        })
+    }
+};
