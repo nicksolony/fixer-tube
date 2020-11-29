cb55cf1d402b12b06f6eca79821c2b9f5854ee36
Nick <github email address>
2020-11-28 19:02:06 -0500

    started working on new video form

--- fixer-tube-frontend/src/components/NewVideo.js
+++ fixer-tube-frontend/src/components/NewVideo.js
@@ -1,15 +1,44 @@
 import React, {Component} from 'react';
+import MainCategories from "./MainCategories"
 import { connect } from "react-redux";
 
 class NewVideo extends Component {
-    render() {
+
+    state = {
+        name:'',
+        description:'',
+        url:'',
+        brandId:'',
+        categoryId:''
+    }
+
+    handleChange = (e) => {
+        this.setState({[e.target.name]: e.target.value}, () => console.log(this.state))
+    }
+
+    render(props) {
         return(
             <div>
-                <form>
-                    <label>New Video Name</label>
-                    <input type="text" name="name" placeholder="type name here"></input>
-                    <input type="submit"/>
-                </form>
+                <h1>this is a site that you can find fixes on</h1>
+                <MainCategories />
+                <div className="mainCategoryListing"> 
+                    <h2>Add new video here:</h2>
+                    <form onChange={this.handleChange}>
+                        <table>
+                            <tbody>
+                            <tr>
+                                <td><label>Video Name: </   label></td>
+                                <td><input type="text" name="name" value= {this.state.name}  placeholder="Type name of your video here"/></td>
+                            </tr>
+                            <tr>
+                                <td><label>Video Description: </label></td>
+                                <td><textarea name="description" value= {this.state.description}  placeholder="Type description of your video here"/></td>
+                            </tr>
+                            <tr><td><input type="submit"/></td></tr>
+                            </tbody>
+                        </table>   
+                    </form>
+                </div>
             </div>
         )
     }
--- fixer-tube-frontend/src/components/Video.js
+++ fixer-tube-frontend/src/components/Video.js
@@ -3,43 +3,44 @@ import React, { Component } from 'react'
 import MainCategories from "./MainCategories"
 import { Link } from 'react-router-dom'
 import { connect } from "react-redux";
-
-// import {loadData} from "../redux/actions/videoActions"
-// import { connect } from "react-redux";
+import {loadData} from "../redux/actions/videoActions"
 
 
 const Video = (props) =>{
     
-    const {name, description, url,brand_id,category_id} = props.videos.find(video=>video.slug===props.match.params.slug);
-    const link = `https://www.youtube.com/embed/${url}`
-    const brand=props.brands.find((brand)=>brand.id===brand_id)
-    const category=props.categories.find((category)=>category.id===category_id)
-    
-    return (
-        <div>
-            <h1>this is a site that you can find fixes on</h1>
-            <MainCategories />
-            <h2>{name}</h2>
-            <h3><Link to={`/categories/${category.slug}`}>{category.name}</Link> - <Link to={`/brands/${brand.slug}`}>{brand.name}</Link></h3>
-            <p align="left">{description}</p>
+    if (props.mainCategories==="") {
+        props.loadDataFromDb()
+        return (
+            <div>LOADING...</div>
+        )
+    } else {
 
+        const {name, description, url,brand_id,category_id} =   props.videos.find(video=>video.slug===props.match.params.slug);
+        const link = `https://www.youtube.com/embed/${url}`
+        const brand=props.brands.find((brand)=>brand.id===brand_id)
+        const category=props.categories.find((category) =>category.id===category_id)
+        
+        return (
             <div>
-            <iframe title={name} width="560" height="315" src={link}  frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen="allowFullScreen"></iframe>
+                <h1>this is a site that you can find fixes on</ h1>
+                <MainCategories />
+                <h2>{name}</h2>
+                <h3><Link to={`/categories/${category.slug}`}>  {category.name}</Link> - <Link to={`/brands/${brand.slug}`}>{brand.name}</Link></h3>
+                <p>{description}</p>
+                <iframe title={name} width="560" height="315"   src={link}  frameBorder="0"   allow="accelerometer; autoplay;   clipboard-write; encrypted-media; gyroscope;  picture-in-picture"  allowFullScreen="allowFullScreen"></iframe>
             </div>
-        </div>
-    )
-}
+        )
+    }
+}   
 
 
 const mapStateToProps = (store) => store.main;
 
-// const mapDispatchToProps = (dispatch) => {
-//     return {
-//         loadDataFromDb: () => dispatch(loadData())
-//     }
-//   }
+const mapDispatchToProps = (dispatch) => {
+    return {
+        loadDataFromDb: () => dispatch(loadData())
+    }
+  }
 
-export default connect (mapStateToProps)(Video);
-// export default connect (mapStateToProps,mapDispatchToProps)(Video);
 
-// export default Video;
+export default connect (mapStateToProps,mapDispatchToProps)(Video);
